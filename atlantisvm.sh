8475ec52b4a53466a01c3d7f222db58085fc6972
Hem Nalini Morzaria-Luna <hmorzaria@hotmail.com>
2019-06-07 13:36:23 +0200

    Update atlantisvm.sh
    Added nedit for large files

--- atlantisvm.sh
+++ atlantisvm.sh
@@ -48,7 +48,8 @@ sudo apt-get update -qq && sudo apt-get -y --no-install-recommends install \
     ntp \
     ntpdate \
     subversion \
-    texlive-latex-extra   
+    texlive-latex-extra  \
+    nedit
     
 sudo add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
 sudo apt install gdal-bin python-gdal python3-gdal -y
