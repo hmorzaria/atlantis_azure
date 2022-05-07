bdb6901ebd31c4729cc6979899a29c4bf710f73a
Hem Nalini Morzaria-Luna <hmorzaria@hotmail.com>
2022-05-07 10:51:13 -0700

    Update atlantisvm.sh
    added -y

--- atlantisvm.sh
+++ atlantisvm.sh
@@ -74,18 +74,18 @@ sudo apt autoremove -y #unused packages
 # update indices
 sudo apt update -qq
 # install two helper packages we need
-sudo apt install --no-install-recommends software-properties-common dirmngr
+sudo apt install --no-install-recommends software-properties-common dirmngr -y
 # add the signing key (by Michael Rutter) for these repos
 # To verify key, run gpg --show-keys /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc 
 # Fingerprint: 298A3A825C0D65DFD57CBB651716619E084DAB9
 wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 # add the R 4.0 repo from CRAN -- adjust 'focal' to 'groovy' or 'bionic' as needed
-sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/" -y
 
-sudo apt install --no-install-recommends r-base
-sudo add-apt-repository ppa:c2d4u.team/c2d4u4.0+
+sudo apt install --no-install-recommends r-base -y
+sudo add-apt-repository ppa:c2d4u.team/c2d4u4.0+ -y
 
-sudo apt install --no-install-recommends r-cran-tidyverse
+sudo apt install --no-install-recommends r-cran-tidyverse -y
 
 echo "Install R studio" 
 
