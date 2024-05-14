917921d3870120d362dc23c9c3c7338a1e0a95ee
Hem Nalini Morzaria-Luna <hmorzaria@hotmail.com>
2024-05-14 09:43:55 -0700

    Update manageatlantis.R

--- manageatlantis.R
+++ manageatlantis.R
@@ -14,16 +14,21 @@ registerDoSNOW(cl)
 
 # Run this for loop for one call of model from each cluster, assuming cluster is already initiated. 
 atlantis.scenarios <- foreach(this.index=folder.length, .verbose = TRUE) %dopar% {
-  
+
    this.folder <- folder.paths[this.index]
- 
+
+  #Get model files from blob storage
+  system(paste0("azcopy copy ./",this.folder,"/outputFiles ", "\"https://thisstorageaccount.blob.core.windows.net/atlantis_run_folder_1?SAS keyXXXXXXXX\" --recursive"), wait = TRUE)
+
+system(paste("cd ./", this.folder,";")) 
+  
   # run Atlantis scenario
 system("sudo chmod +x atlantisrunbash.sh; sudo sh ./atlantisrunbash.sh"), wait = TRUE)
   
   done <- as.data.frame("done")
 #use Azcopy to transfer results to blob storage
 
-  azcopy copy "./atlantis_run_folder_1/outputFiles" "https://thisstorageaccount.blob.core.windows.net/atlantis_run_folder_1?SAS keyXXXXXXXX"--recursive
+ system(paste0("azcopy copy "./", this.folder, "/outputFiles" "https://thisstorageaccount.blob.core.windows.net/atlantis_results_folder?SAS keyXXXXXXXX"--recursive"), wait=TRUE)
 
 }
 
